08f763f446bfc923f34f90d289b14be669a1cd18
Наталья Мищенко <mnk96@yandex.ru>
2023-10-13 00:17:49 +0300

    Change compose

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(' ')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 BASE = os.getenv('SECRET_KEY')
 
