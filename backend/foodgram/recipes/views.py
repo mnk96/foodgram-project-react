38f3355faabe51a4d05b88fa47aee874395897b6
Наталья Мищенко <mnk96@yandex.ru>
2023-10-16 23:05:05 +0300

    Ver2

--- backend/foodgram/recipes/views.py
+++ backend/foodgram/recipes/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.decorators import action
-from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
